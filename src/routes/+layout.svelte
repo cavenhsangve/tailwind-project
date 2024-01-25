3c510ddfc1b43fe61901516ffaf960c5d33930ce
cavenhsangve <jadencaven@gmail.com>
2024-01-25 20:57:56 +1100

    html-index

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,32 @@
 <script>
     import "../app.css";
 </script>
-  
+
+<div>
+    <nav>
+        <div>
+            <h1>
+                <a href="/">Food Ninja</a>
+            </h1>
+        </div>
+        <ul>
+            <li>
+                <a href="#">
+                    <span>Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span>About</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <span>Contact</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div> <!-- end nav -->
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+    <div>
+        <a href="#">Log in</a>
+        <a href="#">Sign up</a>
+    </div>
+
+    <header>
+        <h2>Recipes</h2>
+        <h3>For Ninjas</h3>
+    </header>
+
+    <div>
+        <h4>Latest Recipes</h4>
+
+        <div>
+            <!-- cards go here -->
+            <div>
+                <img src="/img/stew.jpeg" alt="stew">
+                <div>
+                    <span>5 Bean Chilli Stew</span>
+                    <span>Recipe by Mariospan>
+                </div>
+            </div>
+        </div>
+
+        <h4>Most Popular</h4>
+
+        <div>
+            <!-- cards go here -->
+        </div>
+
+        <div>
+            <div>Load more</div>
+        </div>
+    </div>
+</main>
